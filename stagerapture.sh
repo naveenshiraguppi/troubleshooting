08ea257565e1fd30480eefcd964cd46eef31c363
naveen.shiraguppi <naveen.shiraguppi@V01ULPT59.corp.intelematics.com>
2017-05-02 12:18:38 +1000

    deleted

--- stagerapture.sh
+++
@@ -1,1 +0,0 @@
-curl -v -d @stagerapturerequest.xml http://rapture-ws-stage.intelematics.com.au/LEXUS_TEST/wrs/ToyotaComm/getData/xml
--- testrapture.sh
+++
@@ -1,1 +0,0 @@
-curl -v -d "<TML><type>request</type><source>HU</source><destination>rapture</destination><transactionId></transactionId><sessionId>273348D5-78BD-4819-AED5-9B3508EF93EC</sessionId><appName>LocalSearch</appName><appVersion>0.10.1</appVersion><command name=\"GetLocalSearchPref\"></command></TML>" http://rapture-ws-tsl-preprod.intelematics.com.au/wrs/LexusComm/getData/xml
