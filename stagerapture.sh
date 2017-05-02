ca84a6d0922ccbdc42aae5c3aafa75b580f6fc20
naveen.shiraguppi <naveen.shiraguppi@V01ULPT59.corp.intelematics.com>
2017-05-02 12:14:13 +1000

    initial commit

---
+++ stagerapture.sh
@@ -0,0 +1,1 @@
+curl -v -d @stagerapturerequest.xml http://rapture-ws-stage.intelematics.com.au/LEXUS_TEST/wrs/ToyotaComm/getData/xml
---
+++ testrapture.sh
@@ -0,0 +1,1 @@
+curl -v -d "<TML><type>request</type><source>HU</source><destination>rapture</destination><transactionId></transactionId><sessionId>273348D5-78BD-4819-AED5-9B3508EF93EC</sessionId><appName>LocalSearch</appName><appVersion>0.10.1</appVersion><command name=\"GetLocalSearchPref\"></command></TML>" http://rapture-ws-tsl-preprod.intelematics.com.au/wrs/LexusComm/getData/xml
